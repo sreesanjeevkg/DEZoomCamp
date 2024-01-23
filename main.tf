3425a3159e999636e7fdd54eb5e664c01cd30472
sreesanjeevkg <hbksanjeev@gmail.com>
2024-01-23 11:21:39 -0600

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.13.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "dezoomcamp-412116"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "test" {
+  name          = "test-bucket-for-a-day"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
